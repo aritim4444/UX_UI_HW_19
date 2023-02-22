e283f37a8b72cd38e83069b4cf6a0bef5d5b8b34
aritim4444 <112461198+aritim4444@users.noreply.github.com>
2023-02-21 18:35:46 -0600

    j1

--- js/index.js
+++ js/index.js
@@ -42,3 +42,43 @@ $('#voyagerHover').on("mouseleave", function(){
  $('#merchantHover').on("mouseleave", function(){
     $('#merchantHiddenPara').slideUp();
          });
+
+
+// $('.grid-container-largeName').on("mouseenter", function(){
+
+//     $('.grid-item-skills').fadeIn(3000);
+//     // $('#uxDesignSkill').addClass('grid-item-skills-2 h3');
+   
+    
+//          });
+
+//  $('#uxDesignSkill').on("mouseleave", function(){
+//     $('#uxDesignSkill').removeClass('grid-item-skills-2');
+//     $('#uxDesignSkill').removeClass('grid-item-skills-2 h3');
+//         });
+
+// $(".tab2").addClass("active");
+// $(".tab1").removeClass("active");
+
+
+$("#projectbtn").click((function() {
+    $("html").animate(
+      {
+        scrollTop: $(".grid-container-myWork").offset().top
+      },
+      800 //speed
+    );
+  
+}));
+
+//   $(".themeChange").on("click", function(){
+//     if ($(this).hasClass("regular")) {
+//       $("#theme").attr("href", "css/blank.css")
+//     }
+//     else if ($(this).hasClass("blue")){
+//       $("#theme").attr("href", "css/blue.css")
+//     }
+//     else if ($(this).hasClass("dark")){
+//       $("#theme").attr("href", "css/dark.css")
+//     }
+//   });
