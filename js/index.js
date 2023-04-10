2f703fb97605b3ffda689d7d3b1121004189e459
aritim4444 <112461198+aritim4444@users.noreply.github.com>
2023-04-09 23:53:18 -0500

    mobile

--- js/index.js
+++ js/index.js
@@ -50,9 +50,16 @@ $(".arrow").mouseover(function (e) {
           $(this).attr("src", $(this).attr("src").replace("./Images/Navigation (7).png", "./Images/Navigation (5).png"));
       });
 
+ $(".arrow2").mouseover(function (e) {    
+        $(this).attr("src", $(this).attr("src").replace("./Images/Navigation (9).png", "./Images/Navigation (10).png"));
+        }).mouseout(function (e) {
+            $(this).attr("src", $(this).attr("src").replace("./Images/Navigation (10).png", "./Images/Navigation (9).png"));
+        });
+
 
 $('.grid-item-csa-arrow').on("click", function(){
   $('.grid-item-csa-arrow').hide();
+  $('.grid-container-csa-openArea').hide();
       $('.grid-container-csa-display').slideDown();
      
         });
@@ -81,15 +88,72 @@ $('.grid-item-csa-arrow').on("click", function(){
 // $(".tab1").removeClass("active");
 
 
-$("#projectbtn").click((function() {
+$("#t-1").click((function() {
     $("html").animate(
       {
-        scrollTop: $(".grid-container-myWork").offset().top
+        scrollTop: $(".grid-item-largeName-CSA").offset().top
       },
       800 //speed
     );
   
 }));
+
+// $("#first-1").hover((function() {
+//   $(this).text("Hey");
+//   // $("html").animate(
+//   //   {
+//   //     scrollTop: $(".grid-container-csa-openArea-2").offset().top
+//   //   },
+//   //   800 //speed
+//   // );
+
+// }));
+
+$("#t-1").on("mouseenter", function () {
+  $(this).text("INTRODUCTION");
+});
+
+$("#t-1").on("mouseleave", function () {
+  $(this).text("1");
+});
+
+$("#t-2").on("mouseenter", function () {
+  $(this).text("USER RESEARCH");
+});
+
+$("#t-2").on("mouseleave", function () {
+  $(this).text("2");
+});
+
+$("#t-3").on("mouseenter", function () {
+  $(this).text("DEFINITION SYNTHESIS");
+});
+
+$("#t-3").on("mouseleave", function () {
+  $(this).text("3");
+});
+
+
+$("#t-4").on("mouseenter", function () {
+  $(this).text("IDEATION");
+});
+
+$("#t-4").on("mouseleave", function () {
+  $(this).text("4");
+});
+
+
+$("#t-5").on("mouseenter", function () {
+  $(this).text("ITTERATION");
+});
+
+$("#t-5").on("mouseleave", function () {
+  $(this).text("5");
+});
+            
+          
+          
+    
 
 //   $(".themeChange").on("click", function(){
 //     if ($(this).hasClass("regular")) {
@@ -102,4 +166,99 @@ $("#projectbtn").click((function() {
 //       $("#theme").attr("href", "css/dark.css")
 //     }
 //   });
+
+
+
+
+
+$(document).ready(function () {
+
+  
+       
+        
+
+    var previousActiveTabIndex = 0;
+
+
+   
+       
+    
+    // var tab = $(".tab-switcher").data("tab-index"); 
+    // console.log(tab);
+    // if(tab == 0){
+    //     $("#first").addClass("purp");
+    //     $("#first-1").addClass("purp");
+    // }
+
+    // $("#first").addClass("purp");
+    // $("#first-1").addClass("purp");
+
+
+    $(".tabs .tab-switcher").each(function () {
+      if($(this).data("tab-index") == 0){
+        $(this).addClass("purp");
+      }
+     
+
+    });
+ 
+    
+
+    $(".tab-switcher").on('click keypress', function (event) {
+        // event.which === 13 means the "Enter" key is pressed
+
+      
+
+
+        if ((event.type === "keypress" && event.which === 13) || event.type === "click") {
+
+            var tabClicked = $(this).data("tab-index");
+
+            if(tabClicked != previousActiveTabIndex) {
+                $("#allTabsContainer .tab-container").each(function () {
+                    if($(this).data("tab-index") == tabClicked) {
+                       
+                        $(".tab-container").hide();
+                        $(this).show();
+                        previousActiveTabIndex = $(this).data("tab-index");
+                       
+                        return;
+                    }
+                   
+                });
+                $(".tab-switcher").removeClass("purp");
+           
+
+               
+
+                
+
+            }
+            
+  
+            
+        // var tapd = $(".tab-switcher").data("tab-index");
+        var helloWrld = "Hello World!";
+
+      
+
+        $(".tabs .tab-switcher").each(function () {
+          if($(this).data("tab-index") == tabClicked){
+            $(this).addClass("purp");
+          }
+         
+
+        });
+           
+         
+
+        }
+        console.log(previousActiveTabIndex);
+
+
+       
+    });
+
+   
+});
 
